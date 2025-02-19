f375ad152f69163ce170b9f82d6c2f774d10ea93
Christian Pitao <Christian.pitao07@gmail.com>
2025-02-19 21:06:53 +0800

    added myportfolio text

---
+++ my_portfolio/src/App.js
@@ -0,0 +1,12 @@
+import MyHeader from './Components/MyHeaders/MyHeader'; // Match exact filename
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyHeader /> {/* Use PascalCase for React components */}
+    </div>
+  );
+}
+
+export default App;
---
+++ my_portfolio/src/Components/MyHeaders/MyHeader.js
@@ -0,0 +1,12 @@
+import './myheader.css';
+
+
+function MyHeader() { // PascalCase function name
+    return (
+        <div className="myportfolio">
+            <h1>My Portfolio</h1>
+        </div>
+    );
+}
+
+export default MyHeader;
