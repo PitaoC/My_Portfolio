b68149bf9982ceea94a880a9d1c6265df9bf9ed0
PitaoC <christian.pitao07@gmail.com>
2025-09-18 17:58:01 +0800

    change the responsive design

--- New-Portfolio/src/components/MyNames/Mynames.jsx
+++ New-Portfolio/src/components/MyNames/Mynames.jsx
@@ -1,9 +1,24 @@
 import HBackground from '../../assets/HackerBackground.png';
 import CircularProgress from "./CircularProgress";
+import React, { useState, useEffect} from "react";
 
 function Mynames() {
+    function useScreenSize() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+    
+    useEffect(() => {
+        function handleResize() {
+        setIsMobile(window.innerWidth < 1024);
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
+    return isMobile;
+    }
+    const isMobile = useScreenSize();
     return (
+        
         <div
             id="About"
             className="lg:w-screen lg:h-screen md:h-[300vh] h-[200vh] bg-stone-950 lg:bg-cover lg:bg-top md:bg-cover md:bg-bottom bg-cover bg-bottom flex flex-col lg:flex-row"
@@ -11,13 +26,13 @@ function Mynames() {
             >
             <div id="col1" className="md:h-screen h-screen flex-1 flex flex-col justify-center lg:pl-10 text-lg-start text-md-center text-center text-white">
                 <p className="text-3xl font-dmserif ">Hey there,</p>
-                <h className="text-7xl lg:text-7xl font-dmserif pt-1 pb-2">
+                <h className="text-6xl lg:text-7xl font-dmserif pt-1 pb-2">
                 I'm Christian <br /> P. Pitao
                 </h>
-                <h className="text-2xl pt-2 pb-3 lg:text-4xl font-bold font-playfair text-red-600">
+                <h className="text-xl pt-2 pb-3 lg:text-4xl font-bold font-playfair text-red-600">
                 - Aspiring Junior Developer
                 </h>
-                <p className="lg:text-base text-sm pt-3 pb-2 font-medium italic font-roboto tracking-wide lg:max-w-2xl text-md-justify text-justify px-lg-0 px-md-5 px-4">
+                <p className="lg:text-base text-xs pt-3 pb-2 font-medium italic font-roboto tracking-wide lg:max-w-2xl text-md-justify text-justify px-lg-0 px-md-5 px-4">
                 A Junior Developer with experience in front-end and back-end
                 development. Passionate about creating efficient, user-friendly
                 applications and eager to contribute to innovative projects while
@@ -28,24 +43,24 @@ function Mynames() {
             <div id="col2" className="h-screen flex-1 flex flex-col lg:items-end items-center justify-center lg:pr-10">
                 <div className='flex lg:flex-row flex-col lg:pt-3 lg:pb-3 my-2 lg:my-0' >
                     <div className='flex flex-col justify-center lg:items-end item-start lg:pr-10 text-center'>
-                        <h className='text-white font-lora text-3xl font-semibold'>Web Development</h>
+                        <h className='text-white font-lora lg:text-3xl text-2xl font-semibold'>Web Development</h>
                         <p className='text-white font-anek-telugu text-xs p-2'>REACT, HTML, CSS, JSX, NODE.js, PHP, SQL, LARAVEL</p>
                     </div>
-                    <CircularProgress value={90} />
+                    <CircularProgress value={90} size={isMobile ? 120 : 160} />
                 </div>
                 <div className='flex lg:flex-row flex-col lg:pt-3 lg:pb-3 my-2 lg:my-0' >
                     <div className='flex flex-col justify-center lg:items-end item-start lg:pr-10 text-center'>
-                        <h className='text-white font-lora text-3xl font-semibold'>Software Development</h>
+                        <h className='text-white font-lora lg:text-3xl text-2xl font-semibold'>Software Development</h>
                         <p className='text-white font-anek-telugu text-xs p-2'>PYTHON, NODE.js, JAVA, MONGODB, API</p>
                     </div>
-                    <CircularProgress value={70} />
+                    <CircularProgress value={70} size={isMobile ? 120 : 160} />
                 </div>
                 <div className='flex lg:flex-row flex-col lg:pt-3 lg:pb-3 my-2 lg:my-0' >
                     <div className='flex flex-col justify-center lg:items-end item-start lg:pr-10 text-center'>
-                        <h className='text-white font-lora text-3xl font-semibold'>Application Development</h>
+                        <h className='text-white font-lora lg:text-3xl text-2xl font-semibold'>Application Development</h>
                         <p className='text-white font-anek-telugu text-xs p-2'>REACT NATIVE, KOTLIN, JAVASCRIPT, JAVA</p>
                     </div>
-                    <CircularProgress value={80} />
+                    <CircularProgress value={80} size={isMobile ? 120 : 160} />
                 </div>
             </div>
         </div>
--- New-Portfolio/src/components/Mycontacts.jsx
+++ New-Portfolio/src/components/Mycontacts.jsx
@@ -33,23 +33,23 @@ function Mycontacts(){
             <div id="ContactPadding" className='flex lg:flex-1 items-center justify-center
                                                 w-screen h-screen'>
                 <form ref={form} id="FillupForm" onSubmit={sendEmail} className='lg:w-2xl w-8/10 
-                                                                                 font-libertinus-sans text-lg font-medium'>
+                                                                                 font-libertinus-sans lg:text-lg text-sm font-medium'>
                     <p className=' my-3'>Name</p>
-                    <input id='NameIn' className='w-full h-10  rounded-md bg-gray-700 pl-3' type="text" name="user_name" placeholder="Enter Name" required />
+                    <input id='NameIn' className='w-full h-10 text-xs placeholder:text-sm rounded-md bg-gray-700 pl-3' type="text" name="user_name" placeholder="Enter Name" required />
 
                     <div id='Email-Phone-box' className='flex flex-row'>
                         <div id='Email' className='flex flex-1 flex-col mr-2'>
                             <p className='b my-3'>Email</p>
-                            <input id='EmailIn' className='w-full h-10  rounded-md bg-gray-700 pl-3' type="email" name="user_email" placeholder="Enter Email" required />
+                            <input id='EmailIn' className='w-full h-10 text-sm placeholder:text-sm rounded-md bg-gray-700 pl-3' type="email" name="user_email" placeholder="Enter Email" required />
                         </div>
 
                         <div id='Phone' className=' flex flex-1 flex-col'>
                             <p className=' my-3'>Phone Number</p>
-                            <input id='PhoneIn' className='w-full h-10 b rounded-md bg-gray-700 pl-3' type="tel" name="user_phone" placeholder="Enter Phone Number" required />
+                            <input id='PhoneIn' className='w-full h-10 text-sm placeholder:text-sm rounded-md bg-gray-700 pl-3' type="tel" name="user_phone" placeholder="Enter Phone Number" required />
                         </div>
                     </div>
                     <p className=' my-3'>Message</p>
-                    <textarea id='TextAreaIn' className='w-full h-60  rounded-md bg-gray-700 pl-3 pt-1' name="message" placeholder="Enter Message" required></textarea>
+                    <textarea id='TextAreaIn' className='w-full h-60 text-sm placeholder:text-sm rounded-md bg-gray-700 pl-3 pt-1' name="message" placeholder="Enter Message" required></textarea>
                     <button id='SubmitButton' className='w-20 rounded-3 bg-teal-300 hover:bg-teal-500 hover:text-teal-200 text-teal-900 font-semibold font-roboto my-3' type="submit">Submit</button>
                 </form>
             </div>
@@ -57,40 +57,41 @@ function Mycontacts(){
                 <div id='ContactDetailsBox' className='lg:w-2xl'>
                     <header id='ContactUsBox' className='lg:flex lg:flex-row 
                                                          justify-center text-center
-                                                         text-7xl font-mozilla-headline-bold
+                                                         lg:text-7xl text-6xl font-mozilla-headline-bold
                                                          my-4'>
                         <h className=''>Contact&nbsp;</h>
                         <h id='h1white' className='text-teal-300'>Us</h> 
                     </header>
-                    <p id='forquestion' className='lg:w-9/10 w-9/10 justify-self-center font-mozilla-headline-extralight text-center text-xl lg:justify-self-center'>
+                    <p id='forquestion' className='lg:w-9/10 w-9/10 justify-self-center font-mozilla-headline-extralight text-center lg:text-xl text-sm lg:justify-self-center'>
                     For questions, job offers, technical assistance or collaboration opportunities 
                     via the contact information provided.
                     </p>
                     <div id='ContactInfo' className='lg:w-9/10 w-9/10 justify-self-center'>
                         <div id='PhoneIcon' className='flex flex-row my-10'>
-                            <div id='phoneIconContainer' className='w-15 h-15 rounded-full mr-5 flex items-center justify-center bg-teal-300'>
-                                <img src={phoneIcon} alt="phoneIcon" className='w-10 h-10'/>
+                            <div id='phoneIconContainer' className='lg:w-15 lg:h-15 w-12 h-12 rounded-full mr-5 flex items-center justify-center bg-teal-300'>
+                                <img src={phoneIcon} alt="phoneIcon" className='lg:w-10 lg:h-10 w-8 h-8'/>
                             </div>
-                            <p className='flex items-center m-0  text-2xl font-mozilla-headline '>+63 956 8369 352</p>
+                            <p className='flex items-center m-0  lg:text-2xl font-mozilla-headline '>+63 956 8369 352</p>
                         </div>
                         <div id='EmailIcon' className='flex flex-row items-center my-10'>
-                            <div id='emailIconContainer' className='w-15 h-15 rounded-full mr-5
+                            <div id='emailIconContainer' className='lg:w-15 lg:h-15 w-12 h-12 rounded-full mr-5
                                                                     justify-items-center content-center 
                                                                     bg-teal-300 '>
-                                <img src={emailIcon} alt="emailIcon" className='w-10 h-10'/>
+                                <img src={emailIcon} alt="emailIcon" className='lg:w-10 lg:h-10 w-8 h-8'/>
                             </div>
-                            <p id='Email' className='flex items-center m-0  text-2xl font-mozilla-headline '> Christianpitao07@gmail.com</p>
+                            <p id='Email' className='flex items-center m-0  lg:text-2xl font-mozilla-headline '> Christianpitao07@gmail.com</p>
                         </div>
                         <div id='AddressIcon' className='flex flex-row items-center my-10 '>
-                            <div id='addressIconContainer' className='w-15 h-15 rounded-full mr-5
+                            <div id='addressIconContainer' className='lg:w-15 lg:h-15 w-12 h-12 rounded-full mr-5
                                                                       justify-items-center content-center 
                                                                       bg-teal-300 '>
-                                <img src={addressIcon} alt="addressIcon" className='w-10 h-10'/>
+                                <img src={addressIcon} alt="addressIcon" className='lg:w-10 lg:h-10 w-8 h-8'/>
                             </div>
                             <p id='Address' className='flex items-center m-0 
                                                        w-8/10
-                                                       text-2xl font-mozilla-headline text-start 
-                                                       '>3552+5RR Ramonita County Homes, Darasa, Tanauan, Batangas, Phillipines</p>
+                                                       lg:text-2xl font-mozilla-headline text-start 
+                                                       '>
+                            3552+5RR Ramonita County Homes, Darasa, Tanauan, Batangas, Phillipines</p>
                         </div>
                     </div>
                 </div>
